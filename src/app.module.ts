069a606fd5fb04a31bf201b33202631913f18eb7
rosselgalarzarafael <rosselgalarzarafael@gmail.com>
2024-10-31 18:03:17 -0500

    mongose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+
+import { MongooseModule } from '@nestjs/mongoose';
+import { MastersModule } from './modules/masters/masters.module';
+import { CompaniesModule } from './modules/companies/companies.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ MongooseModule.forRoot('mongodb://localhost/nest'), MastersModule, CompaniesModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
