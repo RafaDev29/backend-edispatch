e159dee9f321c051121c3d430a5197b9314b0e77
Rafael <rosselgalarzarafael@gmail.com>
2024-11-02 22:43:35 -0500

    billing

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MastersModule } from './modules/masters/masters.module';
 import { CompaniesModule } from './modules/companies/companies.module';
+import { BillingModule } from './modules/billing/billing.module';
 
 
 @Module({
-  imports: [ MongooseModule.forRoot('mongodb://localhost/nest'), MastersModule, CompaniesModule],
+  imports: [ MongooseModule.forRoot('mongodb://localhost/nest'), MastersModule, CompaniesModule, BillingModule],
   controllers: [],
   providers: [],
 })
